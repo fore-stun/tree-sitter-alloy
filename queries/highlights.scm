5d277c5500e77f872006559b622fc78598b72b7f
David Baynard <git@baynard.dev>
2024-03-30 04:07:39 +0000

    Begin highlights file

---
+++ queries/highlights.scm
@@ -0,0 +1,47 @@
+; highlights.scm
+
+"abstract" @keyword
+"after" @keyword
+"all" @keyword
+"always" @keyword
+"and" @keyword
+"as" @keyword
+"assert" @keyword
+"before" @keyword
+"disj" @keyword
+"eventually" @keyword
+"extends" @keyword
+"fact" @keyword
+"fun" @keyword
+"historically" @keyword
+"iden" @keyword
+"iff" @keyword
+"implies" @keyword
+"in" @keyword
+"let" @keyword
+"lone" @keyword
+"module" @keyword
+"no" @keyword
+"none" @keyword
+"not" @keyword
+"once" @keyword
+"one" @keyword
+"open" @keyword
+"or" @keyword
+"pred" @keyword
+"releases" @keyword
+"set" @keyword
+"sig" @keyword
+"since" @keyword
+"some" @keyword
+"sum" @keyword
+"triggered" @keyword
+"univ" @keyword
+"until" @keyword
+"var" @keyword
+
+(alloyModule
+  (import
+    module: (qualName
+      prefix: (name)
+      name: (name)) @module))
