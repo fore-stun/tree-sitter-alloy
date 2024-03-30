e6e033bca6f3e3911ec6264a934cef40d1d8bc68
David Baynard <git@baynard.dev>
2024-03-30 04:14:32 +0000

    Add unsupported keywords

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -8,18 +8,28 @@
 "as" @keyword
 "assert" @keyword
 "before" @keyword
+; "but" @keyword
+; "check" @keyword
 "disj" @keyword
+; "else" @keyword
+; "enabled" @keyword
+; "event" @keyword
 "eventually" @keyword
+; "exactly" @keyword
 "extends" @keyword
 "fact" @keyword
+; "for" @keyword
 "fun" @keyword
 "historically" @keyword
 "iden" @keyword
 "iff" @keyword
 "implies" @keyword
 "in" @keyword
+; "Int" @keyword
+; "invariant" @keyword
 "let" @keyword
 "lone" @keyword
+; "modifies" @keyword
 "module" @keyword
 "no" @keyword
 "none" @keyword
@@ -30,10 +40,12 @@
 "or" @keyword
 "pred" @keyword
 "releases" @keyword
+; "run" @keyword
 "set" @keyword
 "sig" @keyword
 "since" @keyword
 "some" @keyword
+; "steps" @keyword
 "sum" @keyword
 "triggered" @keyword
 "univ" @keyword
